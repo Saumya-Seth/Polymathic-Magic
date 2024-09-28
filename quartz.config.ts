1b8f6761e00157aeddc80fb16dfba3bea4de534b
Saumya Seth <saumyaseth01@gmail.com>
2024-09-28 20:09:54 +0530

    name

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "The Hidden Magic Lab ✩‧₊˚༺☆༻*ੈ✩‧",
+    pageTitle: "Polymathic Magic ✩₊˚༺☆༻*ੈ✩‧",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
