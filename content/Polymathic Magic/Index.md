da399a43b4a8b32bcb2868f3722a207e4de0ee4f
Saumya Seth <saumyaseth01@gmail.com>
2024-09-30 17:14:09 +0530

    additions

--- content/Polymathic Magic/Index.md
+++ content/Polymathic Magic/Index.md
@@ -1,7 +1,7 @@
 ---
 longform:
   format: scenes
-  title: Polymathic Magic
+  title: Notes
   workflow: Default Workflow
   sceneFolder: /
   scenes: []
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Polymathic Magic ✩₊˚༺☆༻*ੈ✩‧",
+    pageTitle: "Polymathic Magic ༺☆༻",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
